2c4e5bc67b9303ebcad398f6027ffd5174a394fc
Ian Zaniolo <ianzanisirbone@usp.br>
2025-06-16 16:16:31 -0300

    -Added semaphore to initialize tcp_tls variables.

--- components/tcp_tls/tcp_tls.c
+++ components/tcp_tls/tcp_tls.c
@@ -3,10 +3,14 @@
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/semphr.h"
 #include "esp_log.h"
 #include "lwip/sockets.h"
 #include "esp_tls.h"
 #include "tcp_tls.h"
+
+
+#define COUNT_NEEDED_TO_START_TCP_SOCKET      (2U)
 
 
 typedef struct {
@@ -19,6 +23,9 @@ static const char *tag = "TCP_TLS";
 
 static crypt_buffer_t server_crt = {};
 static crypt_buffer_t server_key = {};
+static bool has_server_crt_set = false;
+static bool has_server_key_set = false;
+static SemaphoreHandle_t semphr_sync = NULL;
 
 /* ------------------- Private Functions ------------------- */
 static void tcp_tls_task(void * params);
@@ -26,34 +33,56 @@ static void tcp_tls_task(void * params);
 
 void tcp_tls_init(void)
 {
-    ESP_LOGI(tag, "----- Initializando tcp_tls task -----");
+    semphr_sync = xSemaphoreCreateCounting(COUNT_NEEDED_TO_START_TCP_SOCKET, 0);
+    if (semphr_sync == NULL)
+    {
+        ESP_LOGE(tag, "----- Error creating sync semaphore -----");
+        return;
+    }
+
+    ESP_LOGI(tag, "----- Initializing tcp_tls task -----");
     xTaskCreate(tcp_tls_task, "tcp_tls_task", 4096, NULL, 4, NULL);
 }
 
 void tcp_tls_set_server_ctr(const uint8_t *crt, const size_t len)
 {
-    if (len > TCP_TLS_MAX_BUFFER_LEN)
+    if ((has_server_crt_set == true) || (len > TCP_TLS_MAX_BUFFER_LEN))
     {
         return;
     }
 
     memcpy(server_crt.val, crt, len);
     server_crt.len = len;
+
+    has_server_crt_set = true;
+    xSemaphoreGive(semphr_sync);
+
+    ESP_LOGI(tag, "----- Server certificate has set -----");
 }
 
 void tcp_tls_set_server_key(const uint8_t *key, const size_t len)
 {
-    if (len > TCP_TLS_MAX_BUFFER_LEN)
+    if ((has_server_key_set == true) || (len > TCP_TLS_MAX_BUFFER_LEN))
     {
         return;
     }
 
     memcpy(server_key.val, key, len);
     server_key.len = len;
+
+    has_server_key_set = true;
+    xSemaphoreGive(semphr_sync);
+
+    ESP_LOGI(tag, "----- Server key has set -----");
 }
 
 static void tcp_tls_task(void * params)
 {
+    while (uxSemaphoreGetCount(semphr_sync) != COUNT_NEEDED_TO_START_TCP_SOCKET)
+    {
+        vTaskDelay(pdMS_TO_TICKS(250));
+    }
+    
     ESP_LOGI(tag, "----- Creating socket -----");
     int listen_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
 
--- main/main.c
+++ main/main.c
@@ -9,9 +9,9 @@ static const char *tag = "MAIN";
 
 void app_main(void)
 {
+    tcp_tls_init();
+    
     sys_initializer_init();
     
     wifi_ap_init();
-
-    tcp_tls_init();
 }
